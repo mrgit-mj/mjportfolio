ea9a80c7c3c668e8b284e7eba05748dc03e060ec
Mohamed Aljamil <mohd.jj89@gmail.com>
2020-05-01 12:02:52 +0800

    adding email

--- js/contactus.js
+++ js/contactus.js
@@ -1,27 +1,29 @@
 
         function submit() {
             
-            $.get('https://geoip-db.com/json/', data => {
-                sendEmail(JSON.parse(data))
-            });
+            // $.get('https://geoip-db.com/json/', data => {
+            //     sendEmail(JSON.parse(data))
+            // });
             
             
-            function sendEmail(geoip) {
+            function sendEmail() {
                 let respMessage = `<div id="messageHolder" class="alert alert-success" role="alert">
                   Thanks for contacting me. I will get back to you as soon as possible
                 </div>`
+                let btn = document.getElementById('submit-btn');
+                btn.disabled = true;
                 let form = document.getElementById('form');
                 let name = document.getElementById('name').value;
                 let email = document.getElementById('email').value;
                 let subject = document.getElementById('subject').value;
                 let message = document.getElementById('message').value;
-                
-                $.post('http://Spanarestful-env-1.ghqmmhq8xz.eu-north-1.elasticbeanstalk.com/api/contact', {name, email, subject, message, geoip})
+                let link = 'https://asia-east2-hannyswings.cloudfunctions.net/api/email'
+                $.post(link, {name, email, subject, message})
                 .done(response => {
                     form.innerHTML = '';
                     form.innerHTML = respMessage;
                 })
             }     
-            
+            sendEmail();
         }
     
